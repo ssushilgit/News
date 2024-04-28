2e6d59abab8e6b7b09cbc6520ae9b1962319414f
Sushil Shrestha <165645824+ssushilgit@users.noreply.github.com>
2024-04-28 13:49:13 +0545

    Set Title and Infinte Scroll bar

--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinners from './Spinners'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 
 export class News extends Component {
     static defaultProps = {
-        country :'in',
+        country: 'in',
         pageSize: 6,
         category: 'general'
 
@@ -16,73 +17,106 @@ export class News extends Component {
         pageSize: PropTypes.number,
         category: PropTypes.string
     }
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
 
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         console.log("This is awesome")
         this.state = {
             // articles: this.articles, //imp
             articles: [],
-            loading: false ,
-            page: 1
+            loading: true,
+            page: 1,
+            totalResults: 0
         }
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
     }
-    async updateNews(){
+    async updateNews() {
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=16541d3b22a74a0fa264fe176299cfb5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true})
+        // this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData)
-        this.setState({ articles: parsedData.articles, 
+        this.setState({
+            articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
-         });
+        });
     }
 
     async componentDidMount() {
-      this.updateNews();
+        this.updateNews();
     }
 
-    handlePrevClick = async () => {
-        this.setState({page: this.state.page -1})
-        this.updateNews();
-    }
-    handleNextClick = async () => {
+    fetchMoreData = async() =>{
         this.setState({page: this.state.page +1})
-        this.updateNews();
-    }
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=16541d3b22a74a0fa264fe176299cfb5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true })
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData)
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults,
+            loading: false
+        });
+
+    };
+
+    // handlePrevClick = async () => {
+    //     this.setState({ page: this.state.page - 1 })
+    //     this.updateNews();
+    // }
+    // handleNextClick = async () => {
+    //     this.setState({ page: this.state.page + 1 })
+    //     this.updateNews();
+    // }
     render() {
         return (
-            <div className="container my-3">
-                <h2 className="text-center">NewsMonkey - Top News Recently WorldWide</h2>
-                {this.state.loading&&<Spinners/>}
-                <div className="row mb-3 mt-3" >
-                    {!this.state.loading&&this.state.articles.map((element) => {
-                        return <div className="col-md-4 " key={element.url}>
-                            <NewsItem 
-                             title={element.title ? element.title.slice(0, 72) : ''}
-                             description={element.description ? element.description.slice(0, 120) : ''} 
-                             imgUrl={element.urlToImage} 
-                             newsUrl={element.url}
-                             author={element.author}
-                             date={element.publishedAt}
-                             source={element.source.name} />
-                            {/* We can use '.slice(0, 72)' to limit the display  */}
-                        </div>
-                    })}
+            <>
+                <h2 className="text-center">NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines </h2>
+                {this.state.loading && <Spinners />}
 
-                    <div className="container" style={{ display: 'flex', justifyContent: 'space-between' }}>
 
-                        <button disabled={this.state.page <= 1} type="button" className="btn btn-primary" onClick={this.handlePrevClick}>
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length!== this.state.totalResults}
+                    loader={<Spinners/>}
+                >
+                    <div className="container">
+                    <div className="row mb-3 mt-3" >
+                        {/* !this.state.loading&& */}
+                        {this.state.articles.map((element) => {
+                            return <div className="col-md-4 " key={element.url}>
+                                <NewsItem
+                                    title={element.title ? element.title.slice(0, 72) : ''}
+                                    description={element.description ? element.description.slice(0, 120) : ''}
+                                    imgUrl={element.urlToImage}
+                                    newsUrl={element.url}
+                                    author={element.author}
+                                    date={element.publishedAt}
+                                    source={element.source.name} />
+                                {/* We can use '.slice(0, 72)' to limit the display  */}
+                            </div>
+                        })}
+                    </div>
+                    </div>
+                </InfiniteScroll>
+
+                {/* <div className="container" style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-primary" onClick={this.handlePrevClick}>
                             &larr; Previous
                         </button>
                         <button disabled={this.state.page >= Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-primary" onClick={this.handleNextClick}>
                             Next &rarr;
                         </button>
 
-                    </div>
-                </div>
-            </div>
+                </div> */}
+            </ >
 
 
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,9 +9,9 @@ export class NewsItem extends Component {
                     <img src={!imgUrl ? "https://media.cnn.com/api/v1/images/stellar/prod/phishsphere2024-0418-225029-0799-alivecoverage-enhanced-nr.jpg?c=16x9&q=w_800,c_fill" : imgUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}
-                            <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1'}}>
+                            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1'}}>
                                 {source}
-                            <span class="visually-hidden">unread messages</span>
+                            <span className="visually-hidden">unread messages</span>
                             </span>... </h5>
                         <p className="card-text">{description} ... </p>
                         <p className="card-text"><small className="text-body-secondary">By {!author ? " unknown" : author} on {new Date(date).toGMTString()}</small></p>
